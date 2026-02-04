d99d3748abc7819dc6a26a60ce25395e8681b42d
Naresh Prajapati <np749294@gmail.com>
2026-02-04 12:08:37 +0530

    update learning26 files

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import student,product,sportItem
 # Register your models here.
+
+admin.site.register(student)
+admin.site.register(product)
+admin.site.register(sportItem)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+    #new table 
+class student(models.Model):
+    studentName=models.CharField(max_length=100)
+    studentAge=models.IntegerField()
+    studentCity=models.CharField(max_length=100)
+    studentEmail=models.EmailField(null=True)
+
+    #meta 
+    class meta:
+        db_table='student' #table name 
+
+    #new table 
+class product(models.Model):
+    productName=models.CharField(max_length=100)
+    productPrice=models.IntegerField()
+    productDescription=models.CharField(max_length=50)
+    productStock=models.PositiveIntegerField()
+    productColor=models.CharField(max_length=50,null=True)
+    productStatus=models.BooleanField(default=True)
+
+    class meta :
+        db_table='product' 
+
+    #new table 
+class sportItem(models.Model):
+    sportitemName=models.CharField(max_length=100)
+    sportitemPrice=models.IntegerField()
+    sportitemDescription=models.CharField(max_length=100)
+    sportitemQuntity=models.IntegerField()
+    sportitemColor=models.CharField(max_length=100,null=True)
+
+    class meta:
+        db_table='sportitem'
